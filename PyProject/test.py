132650ea4c0d6a62fb990678bd74cdc3c2dccbb7
digitalsin <digitalsin@gmx.com>
2019-01-29 22:31:09 +0300

    Mod: Pledge Working, Pledger List Working

--- PyProject/test.py
+++ PyProject/test.py
@@ -4,10 +4,9 @@ from flask_wtf import Form
 from wtforms import TextField, IntegerField, SubmitField
 from wtforms.validators import DataRequired, NumberRange
 from redis import Redis
-
-import random
-import string
-
+from operator import itemgetter
+from datetime import datetime
+import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'why do I need this again?'
@@ -26,16 +25,22 @@ class pledges(Form):
 
     def save_to_redis(self):
         counter = self.r.incr('pledge_count')
-        self.r.rpush('pledger:' + str(counter), self.name.data)
-        self.r.rpush('pledger:' + str(counter), self.loc.data)
-        self.r.rpush('pledger:' + str(counter), self.amount.data)
+        # change json to string
+        pl_str = json.dumps({'id': str(counter),
+                             'time': str(datetime.now().strftime('%Y-%b-%d %H:%M:%S')),
+                             'name': self.name.data,
+                             'loc': self.loc.data,
+                             'loot': self.amount.data})
+        self.r.sadd('pledger', pl_str )
 
-    # Change keys
+    # Sort keys in reverse order
     def get_redis(self):
-        pledgers_ledger = {}
-        for item in self.r.keys('pledger:*'):
-            pledgers_ledger[item] = (self.r.lrange(str(item),0,2))
-        return pledgers_ledger
+        pledgers_ledger = []
+        for item in self.r.smembers('pledger'):
+            pledgers_ledger.append(json.loads(item))
+
+        # Sort the list in reverse
+        return sorted(pledgers_ledger, key=itemgetter('id'), reverse=True)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -43,13 +48,17 @@ class pledges(Form):
 def hello_world():
     form = pledges()
     base_element = {'title': 'Pledge Simulator', 'author': 'digitalsin', 'compliment': ''}
-    pledger_journal = form.get_redis()
     if form.validate_on_submit():
         form.save_to_redis()
         base_element['compliment'] = 'Pledge recorded. Thanks!'
-        print(pledger_journal)
+
+    # Read redis and dump list
+    pledger_journal = form.get_redis()
+    print(datetime.now())
+    print(pledger_journal)
+
     form.clear_form()
-    return render_template('index.html', base = base_element, form=form)
+    return render_template('index.html', base = base_element, form=form, pledger_list=pledger_journal)
 
 
 if __name__ == "__main__":
